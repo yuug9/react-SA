7c241cbcebaf8e9e8681a50e609cdf9eb2f57f72
dtmkeng <kang081@hotmail.com>
2017-10-14 11:37:26 +0700

    add controller save

--- src/main/java/com/example/demo/ReactBootApplication.java
+++ src/main/java/com/example/demo/ReactBootApplication.java
@@ -1,12 +1,10 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class DemoApplication {
+public class ReactBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ReactBootApplication.class, args);
 	}
 }
