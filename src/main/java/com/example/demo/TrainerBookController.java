144620fe016c5b4a26475b082bc91fb4176ff55b
dtmkeng <kang081@hotmail.com>
2017-10-17 01:29:14 +0700

    change name

--- src/main/java/com/example/demo/TrainerBookController.java
+++ src/main/java/com/example/demo/TrainerBookController.java
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 @Controller
-public class MakeTrainerController {
+public class TrainerBookController {
 
     @Autowired
-    ChooseTrainerRepository chooseTrainerRepository;
+    TrainerBookRepository trainerBookRepository;
     @ResponseBody
     @RequestMapping(path = "/name/{name}/date/{date}/time/{time}/type/{typeTraniner}/trainer/{traninerName}", method = RequestMethod.GET)
     public String chooseTrainer(@PathVariable String name,@PathVariable String date,@PathVariable String time,@PathVariable String typeTraniner,@PathVariable String traninerName) {
-        ChooseTrainer chooseTrainer = new ChooseTrainer(name,date,time,typeTraniner,traninerName);
-        this.chooseTrainerRepository.save(chooseTrainer);
+        TrainerBook trainerBook = new TrainerBook(name,date,time,typeTraniner,traninerName);
+        this.trainerBookRepository.save(trainerBook);
         return "{\"status\":\"Voted\"}";
     }
 }
--- src/main/java/com/example/demo/TrainerBookRepository.java
+++ src/main/java/com/example/demo/TrainerBookRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
-public interface ChooseTrainerRepository extends CrudRepository<ChooseTrainer, Long> {
+public interface TrainerBookRepository extends CrudRepository<TrainerBook, Long> {
     
 }
